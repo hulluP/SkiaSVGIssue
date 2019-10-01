f131c6b7e065bf0c87a446b32040eb750f5051c7
hulluP <hulluperuna@gmx.net>
2019-10-01 11:16:01 +0200

    initial Checking

---
+++ debugSVG/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace debugSVG
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void PressMeButton_Clicked(object sender, EventArgs e)
+        {
+            var ccView = new SvgView();
+            var page = ccView.GetMainPage();
+            var ccPage = new NavigationPage(page);
+            Navigation.PushModalAsync(ccPage);
+
+        }
+    }
+}
---
+++ debugSVG/SvgView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace debugSVG
+{
+    public class SvgView : ContentPage
+    {
+        private readonly SKCanvasView _canvasView = new SKCanvasView();
+
+        public SvgView()
+        {
+        }
+        public Page GetMainPage()
+        {
+
+            _canvasView.PaintSurface += CanvasViewOnPaintSurface;
+            Content = _canvasView;
+            return this;
+        }
+        private void CanvasViewOnPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKCanvas canvas = args.Surface.Canvas;
+            canvas.Clear();
+            using (Stream stream = GetType().Assembly.GetManifestResourceStream("debugSVG.demo.svg"))
+            {
+                SKSvg svg = new SKSvg();
+                svg.Load(stream);
+                canvas.DrawPicture(svg.Picture);
+            }
+
+        }
+    }
+}
